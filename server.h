92a3f0959ef48e7379ee1508a3d7698947a93378
sierramike <sierramike.fr+github@gmail.com>
2016-09-20 21:18:46 +0200

    Temporary work

--- server.h
+++ server.h
@@ -4,5 +4,6 @@
 void* ServerThread(void* ptr);
 bool CheckMessage (char* message, int size);
 int ProcessMessage(char* message, char* buffer_out);
+ModuleIdent*  GetModuleIdent(int addr);
 
 #endif /* SERVER_H */
